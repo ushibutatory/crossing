b0127ffef3ecb60fefaa66d309dc4521345008e9
ushibutatory <ushibutatory@gmail.com>
2021-01-28 01:51:14 +0900

    comment out: too heavy

--- src/CrossingSolution/Crossing.Tests/Counter/BubbleSortTest.cs
+++ src/CrossingSolution/Crossing.Tests/Counter/BubbleSortTest.cs
@@ -12,7 +12,7 @@ namespace Crossing.Tests.Counter
         [InlineData("data/sample5.txt", 4)]
         [InlineData("data/sample3000.txt", 2217109)]
         [InlineData("data/sample10000.txt", 25029255)]
-        //[InlineData("data/crossing.txt", 275115831438)] 重すぎるのでコメントアウト
+        //[InlineData("data/crossing.txt", 275115831438)] // 重すぎるのでコメントアウト
         public void Count(string filePath, long expected)
         {
             var actual = _counter.Count(filePath);
